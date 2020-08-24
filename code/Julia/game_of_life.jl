d5b77da165be4d6a4c3bd99cfccf6c6a44db2341
oaaky <omkar.omkar.135@gmail.com>
2020-08-24 17:37:30 -0400

    adding one compile time and one logical error

--- code/Julia/game_of_life.jl
+++ code/Julia/game_of_life.jl
@@ -12,21 +12,21 @@ function print_grid(grid,row,col)
     end
 end
 
-function game_of_life(row,col, iteration)
+function game_of_life(row, col, iteration, threshold)
     grid = zeros(Int32, row+2, col+2)
-    grid[2:row+1,2:col+1] = map(x -> rand()>.6 ? 1 : 0, grid[2:row+1,2:col+1])
+    grid[2:row+1,2:col+1] = map(x -> rand()>threshold ? 1 : 0, grid[2:row+1,2:col+1])
     println("Initial State")
     print_grid(grid,row,col)
     for t in 1:iteration
         new_grid = zeros(Int32, row+2, col+2)
         for i in 2:row+1
             for j in 2:col+1
-                neighbor = grid[i,j-1] + grid[i,j+1] + grid[i+1,j] + grid[i-1,j] + grid[i-1,j-1] + grid[i-1,j+1]+grid[i+1,j-1] + grid[i+1,j+1]
+                neighbor = grid[i,j-1] + grid[i,j+1] + grid[i+1,j] + grid[i-1,j] + grid[i-1,j-1] + grid[i-1,j+1]+grid[i+1,j-1] + grid[i+1,j+1] + grid[i,j]
                 new_grid[i,j] = grid[i,j]
                 if grid[i,j] == 0
                     new_grid[i,j] = neighbor == 3?1:0
                 else
-                    new_grid[i,j] = neighbor==2 || neighbor== 3 ? 1:0
+                    new_grid[i,j] = neighbor==2 || neighbors == 3 ? 1:0
                 end
             end
             print("\n")
@@ -38,4 +38,4 @@ function game_of_life(row,col, iteration)
     end
 end
 
-game_of_life(10,5,3)
+game_of_life(10, 5, 3, 0.7)
